2820e933582b91dba3583d711ab764a064f0c79f
Papis Alarcon <papis2002@outlook.com>
2021-03-10 17:23:21 -0600

    subiendo archivos

---
+++ src/mx/unam/aragon/fes/persistencia/ArchivoEmpleado.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.unam.aragon.fes.persistencia;
+
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import mx.unam.aragon.fes.Empleado;
+
+/**
+ *
+ * @author papis
+ */
+public class ArchivoEmpleado {
+    private String archivo;
+
+    public ArchivoEmpleado() {
+    }
+
+    public ArchivoEmpleado(String archivo) {
+        this.archivo = archivo;
+    }
+
+    public String getArchivo() {
+        return archivo;
+    }
+
+    public void setArchivo(String archivo) {
+        this.archivo = archivo;
+    }
+    public void guardarEmpleado(ArrayList<Empleado> datos ){
+         try {
+            ObjectOutputStream fSalida = new ObjectOutputStream(
+                    new FileOutputStream(archivo));
+
+            fSalida.writeObject(datos);
+            fSalida.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+    }
+    public ArrayList<Empleado> leerEmpleados(){
+        ArrayList<Empleado> tmp = null;
+                return tmp;
+    }
+}
